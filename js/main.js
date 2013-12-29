11e92a01dda7ec9f5847b9526053e32802ba88ff
3mpire <charleswesley@gmail.com>
2013-12-28 23:43:06 -0800

    Added Google Map to dialog

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ var log = { Refresh: function(){
 				var long = incident[12];
 
 				//incidents.push('<tr><td>' + incidentID + '</td><td>' + type + '</td><td><a href="map.html?id=' + incidentID + '&lat=' + lat + '&long=' + long + '" title="lat: ' + lat + '; long: ' + long + '">' + address + '</a></td><td>' + date.toLocaleTimeString() + ' ' + date.toLocaleDateString() + '</td></tr>');
-				incidents.push('<tr data-toggle="modal" data-target="#incident-modal" data-incidentid="' + incidentID + '" data-incident="' + incident + '" ><td>' + incidentID + '</td><td>' + type + '</td><td>' + address + '</td><td>' + date.toLocaleTimeString() + ' ' + date.toLocaleDateString() + '</td></tr>');
+				incidents.push('<tr data-toggle="modal" data-target="#incident-modal" data-incidentid="' + incidentID + '" data-incident="' + incident + '" data-lat="' + lat + '" data-lng="' + long + '" ><td>' + incidentID + '</td><td>' + type + '</td><td>' + address + '</td><td>' + date.toLocaleTimeString() + ' ' + date.toLocaleDateString() + '</td></tr>');
 			});
 
 			//First remove existing rows.
@@ -46,7 +46,24 @@ var log = { Refresh: function(){
     });
 }, GetIncident: function(incidentId) {
 	//TODO: Get JSON for a single incident.
-} };
+}, GetMap: function(lat, lng, incidentId) {
+	var incidentLatLng = new google.maps.LatLng(lat,lng);
+
+	var mapOptions = {
+	  center: incidentLatLng,
+	  zoom: 16
+	};
+
+	window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	// To add the marker to the map, use the 'map' property
+	var marker = new google.maps.Marker({
+	    position: incidentLatLng,
+	    map: map,
+	    title:("#" + incidentId)
+	});
+}
+};
 
 $(document).ready(function(){
 	log.Refresh();
@@ -60,10 +77,26 @@ $("#refresh").on('click', function(event){
 $('#incident-modal').on('show.bs.modal', function (e) {
   	var incident = $(e.relatedTarget);
   	var incidentId = incident.data('incidentid');
+  	var incidentLat = incident.data('lat');
+  	var incidentLng = incident.data('lng');
   	var incidentData = incident.data('incident');
   	var modalWindowTitle = $('#incident-modal-title');
   	var modalWindowBody = $('#incident-modal-body');
 
+  	log.GetMap(incidentLat, incidentLng, incidentId);
+
   	modalWindowTitle.text(incidentId);
   	modalWindowBody.html(incidentData);
+});
+
+$('#incident-modal').on('shown.bs.modal', function (e) {
+	//Resize: http://stackoverflow.com/questions/11742839/showing-a-google-map-in-a-modal-created-with-twitter-bootstrap
+	//Recenter: http://stackoverflow.com/a/10002547/1754037
+	var incident = $(e.relatedTarget);
+	var incidentLat = incident.data('lat');
+  	var incidentLng = incident.data('lng');
+	var center = new google.maps.LatLng(incidentLat, incidentLng);
+
+    google.maps.event.trigger(document.getElementById("map-canvas"), "resize");
+    window.map.panTo(center);
 });
