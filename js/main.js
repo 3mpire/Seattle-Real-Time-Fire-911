e82f4669731fd963e6ac8e06ee4ea4fc7bd58caa
3mpire <charleswesley@gmail.com>
2013-12-30 17:15:20 -0800

    Refactored passing incident data and separated formatting table.

--- js/main.js
+++ js/main.js
@@ -8,107 +8,120 @@ function Incident(incidentData) {
 	this.Lng = incidentData[12];
 };
 
-var log = { Refresh: function(){
-	var dataSource = "http://data.seattle.gov/api/views/kzjm-xkqj/rows.json?jsonp=?&max_rows=100";
-	var spinner = $('#spinner');
-	var incidents [];
+// Store all fetched incident objects in this global array.
+window.incidents = [];
 
-	$.ajax({
-		type : "GET",
-		dataType : "JSONP",
-		url : dataSource,
-		beforeSend: function () {
-			spinner.slideDown('slow');
-		},
-		success: function(result) {
-			var tableData = [];
-			
-			$.each( result.data, function( i, incidentData ) {
-				var thisIncident = new Incident(incidentData);
-				this.incidents.push(thisIncident);
+var log = { 
+	RefreshData: function() {
+		var dataSource = "http://data.seattle.gov/api/views/kzjm-xkqj/rows.json?jsonp=?&max_rows=100";
+		var spinner = $('#spinner');
 
-				tableData.push('<tr data-toggle="modal" data-target="#incident-modal" data-incident="' + thisIncident + '" data-incidentid="' + thisIncident.ID + '" data-lat="' + thisIncident.Lat + '" data-lng="' + thisIncident.Lng + '" data-type="' + thisIncident.Category + '" data-address="' + thisIncident.Address + '" ><td>' + thisIncident.ID + '</td><td>' + thisIncident.Category + '</td><td>' + thisIncident.Address + '</td><td>' + thisIncident.DateLogged.toLocaleTimeString() + ' ' + thisIncident.DateLogged.toLocaleDateString() + '</td></tr>');
-			});
+		$.ajax({
+			type : "GET",
+			dataType : "JSONP",
+			url : dataSource,
+			beforeSend: function () {
+				spinner.slideDown('slow');
+			},
+			success: function(result) {								
+				$.each( result.data, function( i, incidentData ) {
+					var thisIncident = new Incident(incidentData);
+					window.incidents.push(thisIncident);
+				});
+			},
+			error: function(xhr, status, error) {
+			  	console.log(JSON.parse(xhr.responseText));
+			}
+		}).complete(function(){
+			spinner.slideUp('slow');
+			log.RefreshTable();
+			//Refresh feed every 5 minutes.
+	        setTimeout(function(){log.RefreshData();}, 300000);
+	    });
+	}, 
+	RefreshTable: function () {
+		var tableData = [];
 
-			//First remove existing rows.
-			$("#log").find('tr:gt(0)').remove();
-
-			//Next append new data.
-			$("#log").append(tableData.join(''));
-		},
-		error: function(xhr, status, error) {
-		  	console.log(JSON.parse(xhr.responseText));
+		for (var i = 0; i < window.incidents.length; i++) {
+			var thisIncident = window.incidents[i];
+			tableData.push('<tr data-toggle="modal" data-target="#incident-modal" id="' + thisIncident.ID + '""><td>' + thisIncident.ID + '</td><td>' + thisIncident.Category + '</td><td>' + thisIncident.Address + '</td><td>' + thisIncident.DateLogged.toLocaleTimeString() + ' ' + thisIncident.DateLogged.toLocaleDateString() + '</td></tr>');
 		}
-	}).complete(function(){
-		spinner.slideUp('slow');
-		//Refresh feed every 5 minutes.
-        setTimeout(function(){log.Refresh();}, 300000);
-    });
-}, GetIncident: function(incidentId) {
-	if (this.incidents.length > 0) {
-		//TODO: see if incident exists in array.
+
+		//First remove existing rows.
+		$("#log").find('tr:gt(0)').remove();
+
+		//Next append new data.
+		$("#log").append(tableData.join(''));
+	},
+	GetIncident: function(incidentId) {
+		if (window.incidents.length > 0) {
+			//TODO: see if incident exists in array.
+			for (var i = 0; i < window.incidents.length; i++) {
+				if (window.incidents[i].ID == incidentId) {
+					return incidents[i];
+				}
+			}
+		}
+		else {
+			//TODO: Get JSON for a single incident.
+		}
+		
+		return null;
+	}, 
+	GetMap: function(lat, lng, incidentId) {
+		var incidentLatLng = new google.maps.LatLng(lat,lng);
+
+		var mapOptions = {
+		  center: incidentLatLng,
+		  zoom: 16
+		};
+
+		window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+		// Add Marker.
+		var marker = new google.maps.Marker({
+		    position: incidentLatLng,
+		    animation: google.maps.Animation.DROP,
+		    map: map,
+		    title:("#" + incidentId)
+		});
+
+		// Add InfoWindow.
+		marker.info = new google.maps.InfoWindow({
+		  content: '<b>Incident:</b> ' + incidentId
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+		  marker.info.open(map, marker);
+		});
+
+		// Invert colors.
+		// reference: https://developers.google.com/maps/documentation/javascript/styling
+		var styles = [
+		  {
+		    "stylers": [
+	      		{ "invert_lightness": true }
+	    	]
+		  }
+		];
+
+		map.setOptions({styles: styles});
 	}
-	else {
-		//TODO: Get JSON for a single incident.
-	}
-	
-	return null;
-}, GetMap: function(lat, lng, incidentId) {
-	var incidentLatLng = new google.maps.LatLng(lat,lng);
-
-	var mapOptions = {
-	  center: incidentLatLng,
-	  zoom: 16
-	};
-
-	window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-	// Add Marker.
-	var marker = new google.maps.Marker({
-	    position: incidentLatLng,
-	    animation: google.maps.Animation.DROP,
-	    map: map,
-	    title:("#" + incidentId)
-	});
-
-	// Add InfoWindow.
-	marker.info = new google.maps.InfoWindow({
-	  content: '<b>Incident:</b> ' + incidentId
-	});
-
-	google.maps.event.addListener(marker, 'click', function() {
-	  marker.info.open(map, marker);
-	});
-
-	// Invert colors.
-	// reference: https://developers.google.com/maps/documentation/javascript/styling
-	var styles = [
-	  {
-	    "stylers": [
-      		{ "invert_lightness": true }
-    	]
-	  }
-	];
-
-	map.setOptions({styles: styles});
-}
 };
 
 $(document).ready(function(){
-	log.Refresh();
+	log.RefreshData();
 });
 
 $("#refresh").on('click', function(event){
 	event.preventDefault();
-	log.Refresh();
+	log.RefreshData();
 });
 
 $('#incident-modal').on('show.bs.modal', function (e) {
   	var incidentRow = $(e.relatedTarget);
-  	var incident = JSON.parse(incidentRow.data('incident'));
+  	var incident = log.GetIncident(incidentRow.attr('id'));
   	var modalWindowTitle = $('#incident-modal-title');
-
-  	console.log(incident);
 
   	log.GetMap(incident.Lat, incident.Lng, incident.ID);
   	modalWindowTitle.text(incident.ID + ": " + incident.Category + ", " + incident.Address);
@@ -118,7 +131,7 @@ $('#incident-modal').on('shown.bs.modal', function (e) {
 	//Resize: http://stackoverflow.com/questions/11742839/showing-a-google-map-in-a-modal-created-with-twitter-bootstrap
 	//Recenter: http://stackoverflow.com/a/10002547/1754037
 	var incidentRow = $(e.relatedTarget);
-	var incident = JSON.parse(incidentRow.data('incident'));
+  	var incident = log.GetIncident(incidentRow.attr('id'));
 	var center = new google.maps.LatLng(incident.Lat, incident.Lng);
 
     google.maps.event.trigger(document.getElementById("map-canvas"), "resize");
